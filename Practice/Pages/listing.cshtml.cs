40562948a49a26b8c873237700468a1f6e1f264d
rob-arch <robert081104campos@gmail.com>
2026-07-09 18:55:32 +0800

    added listing

---
+++ Practice/Pages/listing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practice.Pages
+{
+    public class listingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
