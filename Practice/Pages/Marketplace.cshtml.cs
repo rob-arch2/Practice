42e73c0031d75b11199d75c99c47f20904aee74e
rob-arch <robert081104campos@gmail.com>
2026-07-09 18:41:29 +0800

    added marketplace

---
+++ Practice/Pages/Marketplace.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practice.Pages
+{
+    public class MarketplaceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
